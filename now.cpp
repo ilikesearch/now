2aa575928c9b0d9d6f0ab284c5e36c3fb24c61fe
zhh <no@e.mail>
2021-06-17 15:24:40 +0800

    remove function parse args

--- now.cpp
+++ now.cpp
@@ -8,15 +8,6 @@ using namespace std;
 using namespace chrono;
 using namespace cxxopts;
 
-auto parse_args(Options &opts, int argc, char **argv) {
-  try {
-    return opts.parse(argc, argv);
-  } catch (const OptionException &) {
-    cout << opts.help() << endl;
-    exit(-1);
-  }
-}
-
 int main(int argc, char **argv) {
   Options opts("now");
 
@@ -25,7 +16,13 @@ int main(int argc, char **argv) {
       value<string>()->default_value("%Y-%m-%d %X %a"),
       "<string>")("h,help", "print help and exit");
 
-  auto args = parse_args(opts, argc, argv);
+  ParseResult args;
+  try {
+    args = opts.parse(argc, argv);
+  } catch (const OptionException &) {
+    cout << opts.help() << endl;
+    exit(-1);
+  }
 
   if (args.count("help")) {
     cout << opts.help() << endl;
