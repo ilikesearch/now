f9d66271723a4999d1511da4c57e1071f728399d
zhh <no@e.mail>
2021-06-17 14:37:28 +0800

    using namespace

--- now.cpp
+++ now.cpp
@@ -4,34 +4,36 @@
 
 #include "cxxopts.hpp"
 
-using std::chrono::system_clock;
+using namespace std;
+using namespace chrono;
+using namespace cxxopts;
 
-auto parse_args(cxxopts::Options &opts, int argc, char **argv) {
+auto parse_args(Options &opts, int argc, char **argv) {
   try {
     return opts.parse(argc, argv);
-  } catch (const cxxopts::OptionException &) {
-    std::cout << opts.help() << std::endl;
+  } catch (const OptionException &) {
+    cout << opts.help() << endl;
     exit(-1);
   }
 }
 
 int main(int argc, char **argv) {
-  cxxopts::Options opts("now");
+  Options opts("now");
 
   opts.add_options()(
       "f,format", "specify time format",
-      cxxopts::value<std::string>()->default_value("%Y-%m-%d %X %a"),
+      value<string>()->default_value("%Y-%m-%d %X %a"),
       "<string>")("h,help", "print help and exit");
 
   auto args = parse_args(opts, argc, argv);
 
   if (args.count("help")) {
-    std::cout << opts.help() << std::endl;
+    cout << opts.help() << endl;
     exit(0);
   }
 
   auto time = system_clock::to_time_t(system_clock::now());
-  auto format = args["format"].as<std::string>().c_str();
-  auto formatted = std::put_time(std::localtime(&time), format);
-  std::cout << formatted << std::endl;
+  auto format = args["format"].as<string>().c_str();
+  auto formatted = put_time(localtime(&time), format);
+  cout << formatted << endl;
 }
